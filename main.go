16b2121cb7d99c956cd4a703a760837b93ef537c
manzoDev <manzoroger08@gmail.com>
2024-05-07 14:02:10 -0400

    modify title on varchar and use migrate for db

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/manzoDev/go-gorm-restapi/db"
+	"github.com/manzoDev/go-gorm-restapi/models"
 	"github.com/manzoDev/go-gorm-restapi/routes"
 )
 
 func main() {
 
+	//stable connection
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	r := mux.NewRouter()
 
