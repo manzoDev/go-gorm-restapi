25a9334316de20aa02255d0272435cfee42e0503
manzoDev <manzoroger08@gmail.com>
2024-05-07 13:47:58 -0400

    write models data form gorm

---
+++ models/User.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	// esto lee nuestro struct y los convierte en una tabla
+	gorm.Model
+
+	FirstName string `gorm:"not null"`
+	LastName  string `gorm:"not null"`
+	Email     string `gorm:"not null:unique_index"`
+	Tasks     []Task
+}
