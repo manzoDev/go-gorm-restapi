6933a84a9a9b2f76c80ef76552db2e0655e9722d
manzoDev <manzoroger08@gmail.com>
2024-05-08 12:26:11 -0400

    creating handler for route 'User'

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import "net/http"
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetUserHandler"))
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetUserHandler"))
+}
+
+func PostUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetUserHandler"))
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetUserHandler"))
+}
